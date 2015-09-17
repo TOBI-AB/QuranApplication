0d59f06c88fa64622fb74d324831c866a69da929
Abdelghaffar <AbdelghaffarMac@macbook-pro-de-abdelghaffar.local>
2015-09-17 18:14:04 +0100

    Initial Commit

---
+++ QuranApplication/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QuranApplication
+//
+//  Created by Abdelghaffar on 17/09/2015.
+//  Copyright © 2015 Abdelghaffar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
